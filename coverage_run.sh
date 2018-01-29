7dee18b51cbf56f05a53c26cb852afd16f0a4719
CristianRCV <cristianrcv@gmail.com>
2018-01-29 12:49:08 +0100

    Update to new folder structure

--- coverage_run.sh
+++ coverage_run.sh
@@ -15,26 +15,12 @@
   # MAIN
   #
 
-  # Run coverage on decorator
-  (cd decorator || exit 1; run)
-
-  # Run coverage on each translator
-  (cd translators/code_loader || exit 1; run)
-  (cd translators/code_replacer || exit 1; run)
-  (cd translators/py2pycompss || exit 1; run)
-  (cd translators/py2scop || exit 1; run)
-  (cd translators/scop2pscop2py || exit 1; run)
-  (cd translators/scop_types || exit 1; run)
+  # Run coverage on pycompss folder
+  (cd pycompss || exit 1; run)
 
   # Combine all reports
   coverage combine \
-          decorator/.coverage \
-          translators/code_loader/.coverage \
-          translators/code_replacer/.coverage \
-          translators/py2pycompss/.coverage \
-          translators/py2scop/.coverage \
-          translators/scop2pscop2py/.coverage \
-          translators/scop_types/.coverage
+          pycompss/.coverage 
   ev=$?
   if [ "$ev" -ne 0 ]; then
           echo "[ERROR] Coverage combine failed with exit value: $ev"
