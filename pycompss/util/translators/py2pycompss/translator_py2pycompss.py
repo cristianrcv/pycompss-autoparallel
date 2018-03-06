5c1069a9e7964cef553997fa68fa586bab3fc089
CristianRCV <cristianrcv@gmail.com>
2018-03-06 16:59:44 +0100

    Update input and expected for py2pycompss test

--- pycompss/util/translators/py2pycompss/translator_py2pycompss.py
+++ pycompss/util/translators/py2pycompss/translator_py2pycompss.py
@@ -42,6 +42,18 @@ class Py2PyCOMPSs(object):
         if __debug__:
             logger.debug("[Py2PyCOMPSs] Initialize translation")
 
+        # LOG ALL
+        print("----------------------")
+        print("FUNC SOURCE:")
+        print(str(func_source))
+        print("PY FILES:")
+        for par_f in par_py_files:
+                logger.info(" - File: " + str(par_f))
+                with open(par_f, 'r') as f:
+                        print(f.read())
+        print("OUTPUT: " + str(output))
+        print("----------------------")
+
         # TODO: Add real code
         try:
             import os
@@ -82,24 +94,46 @@ class Py2PyCOMPSsException(Exception):
 class TestPy2PyCOMPSs(unittest.TestCase):
 
     def test_matmul(self):
+        # Base variables
         import os
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        src_file = dir_path + "/tests/test1_matmul.src.python"
-        expected_file = dir_path + "/tests/test1_matmul.expected.pycompss"
-        out_file = dir_path + "/tests/test1_matmul.out.pycompss"
+        tests_path = dir_path + "/tests"
+
+        # Insert function file into pythonpath
+        import sys
+        sys.path.insert(0, tests_path)
+
+        # Import function to replace
+        import importlib
+        func_name = "matmul"
+        test_module = importlib.import_module("pycompss.util.translators.py2pycompss.tests.test1_matmul_func")
+        func = getattr(test_module, func_name)
+        import inspect
+        func_code = inspect.getsource(func)
+        
+        # Create list of parallel py codes
+        src_file0 = tests_path + "/test1_matmul.src.python"
+        par_py_files = [src_file0]
+
+        # Output file
+        out_file = tests_path + "/test1_matmul.out.pycompss"
 
         # Translate
-        Py2PyCOMPSs.translate(None, src_file, out_file)
+        Py2PyCOMPSs.translate(func_code, par_py_files, out_file)
 
         # Check file content
-        with open(expected_file, 'r') as f:
-            expected_content = f.read()
-        with open(out_file, 'r') as f:
-            out_content = f.read()
-        self.assertEqual(out_content, expected_content)
-
-        # Erase file
-        os.remove(out_file)
+        expected_file = tests_path + "/test1_matmul.expected.pycompss"
+        try:
+            with open(expected_file, 'r') as f:
+                expected_content = f.read()
+            with open(out_file, 'r') as f:
+                out_content = f.read()
+            self.assertEqual(out_content, expected_content)
+        except Exception:
+            raise
+        finally:
+            # Erase file
+            os.remove(out_file)
 
 
 #
