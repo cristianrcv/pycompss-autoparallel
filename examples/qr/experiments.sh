0f3fb809cd9e7aff43a99fd7b19280ee1b634ec4
CristianRCV <cristianrcv@gmail.com>
2018-04-06 15:18:20 +0200

    Add enqueue and experiment scripts for MN testing

---
+++ examples/qr/experiments.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -e
+
+
+  #
+  # HELPER FUNCTIONS
+  #
+
+  wait_and_get_jobID() {
+    sleep 6s
+    job_dependency=$(squeue | grep "$(whoami)" | sort -n - | tail -n 1 | awk '{ print $1 }')
+    echo "Last Job ID: ${job_dependency}"
+  }
+
+
+  #
+  # MAIN
+  #
+
+  # Script global variables
+  SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+  # Script arguments
+  tracing=${1:-true}
+  graph=${2:-true}
+  log_level=${3:-off}
+
+  # Application variables
+  MSIZES=(16)
+  BSIZES=(1024)
+  NUM_NODES=(2)
+  EXEC_TIMES=(10)
+
+  cpus_per_node=48
+
+  job_dependency=None
+  for i in "${!MSIZES[@]}"; do
+    msize=${MSIZES[$i]}
+    bsize=${BSIZES[$i]}
+    num_nodes=${NUM_NODES[$i]}
+    execution_time=${EXEC_TIMES[$i]}
+
+    for app_version in "${SCRIPT_DIR}"/*/; do
+      echo "--- Enqueueing ${app_version}"
+      (
+      cd "$app_version"
+      ./enqueue.sh "${job_dependency}" "${num_nodes}" "${execution_time}" "${cpus_per_node}" "${tracing}" "${graph}" "${log_level}" "${msize}" "${bsize}"
+      wait_and_get_jobID
+      )
+    done
+  done
