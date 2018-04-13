60e12c389b57c652468654243f36b9669d65fbf9
CristianRCV <cristianrcv@gmail.com>
2018-04-13 14:36:36 +0200

    Add FDTD 2D blocked version (by columns)

---
+++ examples/fdtd-2d-blocked/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+  # Script global variables
+  SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+  for app_version in "${SCRIPT_DIR}"/*/; do
+    echo "--- Executing ${app_version}"
+    (
+    cd "$app_version"
+    ./run.sh "$@"
+    )
+  done
+
