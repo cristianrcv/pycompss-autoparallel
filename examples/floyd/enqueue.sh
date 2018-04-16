10e0e9c68ab80a0eb8093248442df88b96cd885b
CristianRCV <cristianrcv@gmail.com>
2018-04-16 13:54:32 +0200

    Update scripts for MN experimentation

--- examples/floyd/enqueue.sh
+++ examples/floyd/enqueue.sh
@@ -1,50 +1,58 @@
 #!/bin/bash -e                                                                                                                                                                                                                                                                 
                                                                                                                                                                                                                                                                                
-  # Script variables                                                                                                                                                                                                                                                           
-  SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-  EXEC_FILE=${SCRIPT_DIR}/floyd.py
-  WORK_DIR=${SCRIPT_DIR}/results/mn/
-  LOCAL_PYTHONPATH=${SCRIPT_DIR}
-
   # Script arguments
-  if [ $# -ne 0 ] && [ $# -ne 8 ]; then
+  if [ $# -ne 0 ] && [ $# -ne 10 ]; then
     echo "ERROR: Incorrect number of parameters"
     exit 1
   fi
-  job_dependency=${1:-None}
-  num_nodes=${2:-2}
-  execution_time=${3:-10}
-  cpus_per_node=${4:-48}
-  tracing=${5:-false}
-  graph=${6:-false}
-  log_level=${7:-off}
+  app_version=${1:-autoparallel}
+  job_dependency=${2:-None}
+  num_nodes=${3:-2}
+  execution_time=${4:-15}
+  cpus_per_node=${5:-48}
+  tracing=${6:-true}
+  graph=${7:-false}
+  log_level=${8:-off}
 
-  nsize=${8:-1000}
+  nsize=${9:-1000}
+
+  # Script variables                                                                                                                                                                                                                                                           
+  SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  EXEC_FILE=${SCRIPT_DIR}/${app_version}/floyd.py
+  WORK_DIR=${SCRIPT_DIR}/results/mn/${app_version}
+  LOCAL_PYTHONPATH=${SCRIPT_DIR}/${app_version}
+
 
   # Setup Execution environment
   if [ ! -d "${WORK_DIR}" ]; then
-    mkdir "${WORK_DIR}"
+    mkdir -p "${WORK_DIR}"
   fi
 
   export ComputingUnits=1
 
   # Enqueue job
   enqueue_compss \
+    --qos=debug \
     --job_dependency="${job_dependency}" \
     --exec_time="${execution_time}" \
     --num_nodes="${num_nodes}" \
+    \
     --cpus_per_node="${cpus_per_node}" \
-    --log_level="${log_level}" \
+    --worker_in_master_cpus=0 \
+    --node_memory=50000 \
+    \
     --tracing="${tracing}" \
     --graph="${graph}" \
+    --summary \
+    --log_level="${log_level}" \
+    \
     --master_working_dir="${WORK_DIR}" \
     --worker_working_dir=scratch \
     --base_log_dir="${WORK_DIR}" \
-    --lang=python \
-    --node_memory=50000 \
     --pythonpath="${LOCAL_PYTHONPATH}" \
-    --qos=debug \
+    --lang=python \
+    \
     "$EXEC_FILE" "$nsize"
 
-  # Params: job_dependency num_nodes execution_time cpus_per_node tracing graph log_level NSIZE
-  # Example: ./enqueue.sh None 2 10 48 false false off 1000
+  # Params: job_dependency version num_nodes execution_time cpus_per_node tracing graph log_level NSIZE
+  # Example: ./enqueue.sh autoparallel None 2 10 48 false false off 1000
--- examples/floyd/experiments.sh
+++ examples/floyd/experiments.sh
@@ -11,6 +11,15 @@
     echo "Last Job ID: ${job_dependency}"
   }
 
+  log_information() {
+    local job_log_file=$1
+    local job_id=$2
+    local version=$3
+    local n_size=$4
+    local tracing=$5
+
+    echo "$job_id	$version	$n_size	$tracing" >> "${job_log_file}"
+  }
 
   #
   # MAIN
@@ -20,14 +29,19 @@
   SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
   # Script arguments
-  tracing=${1:-true}
-  graph=${2:-true}
-  log_level=${3:-off}
+  job_log_file=${1:-experiments.log}
+
+  # Initialize job log file
+  echo "JOB_ID	VERSION		NSIZE	TRACING" > "${job_log_file}"
 
   # Application variables
-  NSIZES=(1000)
-  NUM_NODES=(2)
-  EXEC_TIMES=(10)
+  graph=false
+  log_level=off
+
+  #        REFERENCE  MAX_PAR
+  NSIZES=(     1000)
+  NUM_NODES=(  2)
+  EXEC_TIMES=( 30)
 
   cpus_per_node=48
 
@@ -37,12 +51,18 @@
     num_nodes=${NUM_NODES[$i]}
     execution_time=${EXEC_TIMES[$i]}
 
-    for app_version in "${SCRIPT_DIR}"/*/; do
+    for app_path in "${SCRIPT_DIR}"/*/; do
+      app_version=$(basename "${app_path}")
       echo "--- Enqueueing ${app_version}"
-      (
-      cd "$app_version"
-      ./enqueue.sh "${job_dependency}" "${num_nodes}" "${execution_time}" "${cpus_per_node}" "${tracing}" "${graph}" "${log_level}" "${nsize}"
+      # With tracing
+      tracing=true
+      ./enqueue.sh "${app_version}" "${job_dependency}" "${num_nodes}" "${execution_time}" "${cpus_per_node}" "${tracing}" "${graph}" "${log_level}" "${nsize}"
       wait_and_get_jobID
-      )
+      log_information "${job_log_file}" "${job_dependency}" "${app_version}" "${nsize}" "${tracing}"
+      # Without tracing
+      tracing=false
+      ./enqueue.sh "${app_version}" "${job_dependency}" "${num_nodes}" "${execution_time}" "${cpus_per_node}" "${tracing}" "${graph}" "${log_level}" "${nsize}"
+      wait_and_get_jobID
+      log_information "${job_log_file}" "${job_dependency}" "${app_version}" "${nsize}" "${tracing}"
     done
   done
