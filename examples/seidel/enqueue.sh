201c2d39afc3d4309c886a719afa770d8e2458d7
CristianRCV <cristianrcv@gmail.com>
2018-04-16 13:55:32 +0200

    Update scripts for MN experimentation

--- examples/seidel/enqueue.sh
+++ examples/seidel/enqueue.sh
@@ -1,51 +1,59 @@
 #!/bin/bash -e                                                                                                                                                                                                                                                                 
                                                                                                                                                                                                                                                                                
-  # Script variables                                                                                                                                                                                                                                                           
-  SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-  EXEC_FILE=${SCRIPT_DIR}/seidel.py
-  WORK_DIR=${SCRIPT_DIR}/results/mn/
-  LOCAL_PYTHONPATH=${SCRIPT_DIR}
-
   # Script arguments
-  if [ $# -ne 0 ] && [ $# -ne 9 ]; then
+  if [ $# -ne 0 ] && [ $# -ne 10 ]; then
     echo "ERROR: Incorrect number of parameters"
     exit 1
   fi
-  job_dependency=${1:-None}
-  num_nodes=${2:-2}
-  execution_time=${3:-10}
-  cpus_per_node=${4:-48}
-  tracing=${5:-false}
-  graph=${6:-false}
-  log_level=${7:-off}
+  app_version=${1:-autoparallel}
+  job_dependency=${2:-None}
+  num_nodes=${3:-2}
+  execution_time=${4:-15}
+  cpus_per_node=${5:-48}
+  tracing=${6:-true}
+  graph=${7:-false}
+  log_level=${8:-off}
 
-  nsize=${8:-1000}
-  tsize=${9:-10}
+  nsize=${9:-1000}
+  tsize=${10:-10}
+
+  # Script variables                                                                                                                                                                                                                                                           
+  SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  EXEC_FILE=${SCRIPT_DIR}/${app_version}/seidel.py
+  WORK_DIR=${SCRIPT_DIR}/results/mn/${app_version}
+  LOCAL_PYTHONPATH=${SCRIPT_DIR}/${app_version}
+
 
   # Setup Execution environment
   if [ ! -d "${WORK_DIR}" ]; then
-    mkdir "${WORK_DIR}"
+    mkdir -p "${WORK_DIR}"
   fi
 
   export ComputingUnits=1
 
   # Enqueue job
   enqueue_compss \
+    --qos=debug \
     --job_dependency="${job_dependency}" \
     --exec_time="${execution_time}" \
     --num_nodes="${num_nodes}" \
+    \
     --cpus_per_node="${cpus_per_node}" \
-    --log_level="${log_level}" \
+    --worker_in_master_cpus=0 \
+    --node_memory=50000 \
+    \
     --tracing="${tracing}" \
     --graph="${graph}" \
+    --summary \
+    --log_level="${log_level}" \
+    \
     --master_working_dir="${WORK_DIR}" \
     --worker_working_dir=scratch \
     --base_log_dir="${WORK_DIR}" \
-    --lang=python \
-    --node_memory=50000 \
     --pythonpath="${LOCAL_PYTHONPATH}" \
-    --qos=debug \
+    --lang=python \
+    \
     "$EXEC_FILE" "$nsize" "$tsize"
 
-  # Params: job_dependency num_nodes execution_time cpus_per_node tracing graph log_level NSIZE TSIZE
-  # Example: ./enqueue.sh None 2 10 48 false false off 1000 10
+  # Params: job_dependency version num_nodes execution_time cpus_per_node tracing graph log_level NSIZE TSIZE
+  # Example: ./enqueue.sh autoparallel None 2 15 48 true false off 1000 10
