327f03323c0b887d8bcc4e186fd9f253768da21c
CristianRCV <cristianrcv@gmail.com>
2018-04-16 13:54:58 +0200

    Update scripts for MN experimentation

--- examples/jacobi-1d/experiments.sh
+++ examples/jacobi-1d/experiments.sh
@@ -11,6 +11,16 @@
     echo "Last Job ID: ${job_dependency}"
   }
 
+  log_information() {
+    local job_log_file=$1
+    local job_id=$2
+    local version=$3
+    local n_size=$4
+    local t_size=$5
+    local tracing=$6
+
+    echo "$job_id	$version	$n_size	$t_size	$tracing" >> "${job_log_file}"
+  }
 
   #
   # MAIN
@@ -20,15 +30,20 @@
   SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
   # Script arguments
-  tracing=${1:-true}
-  graph=${2:-true}
-  log_level=${3:-off}
+  job_log_file=${1:-experiments.log}
+
+  # Initialize job log file
+  echo "JOB_ID	VERSION		NSIZE	TSIZE	TRACING" > "${job_log_file}"
 
   # Application variables
-  NSIZES=(1000)
-  TSIZES=(10)
-  NUM_NODES=(2)
-  EXEC_TIMES=(10)
+  graph=false
+  log_level=off
+
+  #        REFERENCE  MAX_PAR
+  NSIZES=(     1000     40)
+  TSIZES=(     10     100)
+  NUM_NODES=(  2       2)
+  EXEC_TIMES=( 30      30)
 
   cpus_per_node=48
 
@@ -39,12 +54,18 @@
     num_nodes=${NUM_NODES[$i]}
     execution_time=${EXEC_TIMES[$i]}
 
-    for app_version in "${SCRIPT_DIR}"/*/; do
+    for app_path in "${SCRIPT_DIR}"/*/; do
+      app_version=$(basename "${app_path}")
       echo "--- Enqueueing ${app_version}"
-      (
-      cd "$app_version"
-      ./enqueue.sh "${job_dependency}" "${num_nodes}" "${execution_time}" "${cpus_per_node}" "${tracing}" "${graph}" "${log_level}" "${nsize}" "${tsize}"
+      # With tracing
+      tracing=true
+      ./enqueue.sh "${app_version}" "${job_dependency}" "${num_nodes}" "${execution_time}" "${cpus_per_node}" "${tracing}" "${graph}" "${log_level}" "${nsize}" "${tsize}"
       wait_and_get_jobID
-      )
+      log_information "${job_log_file}" "${job_dependency}" "${app_version}" "${nsize}" "${tsize}" "${tracing}"
+      # Without tracing
+      tracing=false
+      ./enqueue.sh "${app_version}" "${job_dependency}" "${num_nodes}" "${execution_time}" "${cpus_per_node}" "${tracing}" "${graph}" "${log_level}" "${nsize}" "${tsize}"
+      wait_and_get_jobID
+      log_information "${job_log_file}" "${job_dependency}" "${app_version}" "${nsize}" "${tsize}" "${tracing}"
     done
   done
